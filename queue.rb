9d90d8b10ca13df64b042300cb7008a965011d87
Sean Chain <seanchain2.0@gmail.com>
2015-01-25 17:06:23 +0800

    Finish constructing the queue data structure using ruby array

--- queue.rb
+++ queue.rb
@@ -1,3 +1,22 @@
 #! /usr/bin/ruby 
 
+class Queue
+	attr_reader :arr
+	def initialize
+		@arr = []
+	end	
+	def enqueue elem
+		@arr.push elem
+	end
+	def dequeue
+		return @arr.shift
+	end
 
+	def queueSize
+		return @arr.size
+	end
+
+	def to_s
+		@arr
+	end
+end
