afffc50ee0788a9daf98a16e58aad242c48a6407
Sean Chain <seanchain2.0@gmail.com>
2015-01-26 22:08:17 +0800

    add api design of union-find.rb

--- union-find.rb
+++ union-find.rb
@@ -1,5 +1,12 @@
 #! /usr/bin/ruby 
 
 #Implementation of union-find algorithms
+#Designed API
+#
+#class UF N 以整数标识初始化的N个触点
+#def union p, q 在p和q之间加上一条连接
+#def find p p所在分量的标识符
+#def connected p, q 查看p和q是否存在于同一分量
+#def count 联通分量的数量
+#
 
-#Still testing the function of it
