60e2e1bb960280b094622edd2d98819451b33dcf
Sean Chain <seanchain2.0@gmail.com>
2015-01-30 23:47:55 +0800

    change file structure

---
+++ Binary Search/binarySearch.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/ruby 
+#
+#THe foundation part of the reference book Algorithm
+#实现二分查找法
+
+
+
+def binarySearch var, ary
+	min, max = 0, ary.length - 1
+	while min <= max
+		mid = min + (max - min) / 2  #首先确定中间数的位置，通过变换中间数的位置来进行查找
+		max = mid - 1 if var < ary[mid]  #中间索引所对应的值大于var，则最大索引等于中间索引-1
+		min = mid + 1 if var > ary[mid]  #中间索引小于var。则最小索引等于中间数+1
+		return mid if var == ary[mid] #相等则返回中间数
+	end
+	return "此数不存在" #找不到，则返回此数不存在
+end
+
+
+ary = [1, 4, 5, 7, 21, 30, 45]
+puts "请输入需要查找的数字"
+var = gets.to_i
+var = binarySearch var, ary
+print "您要找的数字索引为", var, "\n"
---
+++ Linear Structure/bag.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/ruby 
+
+#背包(bag)数据结构的实现
+#背包是一种不支持从中删除元素的集合数据类型，目的是为了帮助用例收集元素并对于元素进行迭代遍历，使用bag说明元素的顺序并不重要
+#
+#通过遍历数字并求得其平均数和方差
+
+num = []
+while var = gets
+	num.push var.chomp!.to_i   #一次保存输入的数字，并消除其换行符，将其转化为数字
+end
+
+sum, sum1 = 0, 0
+for i in num
+	sum += i
+end
+
+average = (Rational sum, num.length).to_f
+num.each do |item|
+	sum1 += (item - average) ** 2
+end
+
+sdev = Math::sqrt(sum1 / (num.length - 1))
+
+printf "Average: %.2f\n", average
+printf "Standard dev: %.2f\n", sdev
