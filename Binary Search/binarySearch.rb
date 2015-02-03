0bbfd34101308f7aa1f4c36b1071388e5bb2b20c
Chen Sihang <seanchain2.0@gmail.com>
2015-02-03 21:20:46 +0800

    coding style

--- Binary Search/binarySearch.rb
+++ Binary Search/binarySearch.rb
@@ -6,14 +6,14 @@
 
 
 def binarySearch var, ary
-	min, max = 0, ary.length - 1
-	while min <= max
-		mid = min + (max - min) / 2  #首先确定中间数的位置，通过变换中间数的位置来进行查找
-		max = mid - 1 if var < ary[mid]  #中间索引所对应的值大于var，则最大索引等于中间索引-1
-		min = mid + 1 if var > ary[mid]  #中间索引小于var。则最小索引等于中间数+1
-		return mid if var == ary[mid] #相等则返回中间数
-	end
-	return "此数不存在" #找不到，则返回此数不存在
+  min, max = 0, ary.length - 1
+  while min <= max
+    mid = min + (max - min) / 2  #首先确定中间数的位置，通过变换中间数的位置来进行查找
+    max = mid - 1 if var < ary[mid]  #中间索引所对应的值大于var，则最大索引等于中间索引-1
+    min = mid + 1 if var > ary[mid]  #中间索引小于var。则最小索引等于中间数+1
+    return mid if var == ary[mid] #相等则返回中间数
+  end
+  return "此数不存在" #找不到，则返回此数不存在
 end
 
 
