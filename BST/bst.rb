84bef12158ff0383e66253e0876becc5dc060e38
Chen Sihang <seanchain2.0@gmail.com>
2015-02-09 16:21:41 +0800

    finish node data structure

--- BST/bst.rb
+++ BST/bst.rb
@@ -2,11 +2,21 @@
 
 
 class Node
-  def initialize
+  attr_accessor :left, :right
+  attr_reader :hash
+  def initialize key, val
+    @hash = Hash.new
+    @hash.store key, val
   end
 end
 
 class BST
   def initialize
+    @root = Node.new
   end
 end
+
+a = Node.new :ruby, 1
+b = Node.new :python, 3
+a.left = b
+p a.left.hash
